4850544c99274c7927b516d9c4d84fcdd5f2eb2a
shima98 <shima.azizimajd98@gmail.com>
2024-01-13 21:32:23 +0330

    add the model

--- src/run.py
+++ src/run.py
@@ -31,3 +31,63 @@ plt.savefig("output_images.png")
 
 # Show a message indicating that the image is saved
 print(f"{num_images_to_display} images saved to output_images.png.")
+
+
+
+#Now we want to Normalize and create model
+
+import mnist
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
+
+
+# Reshape and normalize images
+train_images = train_images.reshape((60000, 28, 28, 1)).astype('float32') / 255
+test_images = test_images.reshape((10000, 28, 28, 1)).astype('float32') / 255
+
+# Split the data into training and validation sets
+train_images, valid_images, train_labels, valid_labels = train_test_split(
+    train_images, train_labels, test_size=0.2, random_state=42
+)
+
+# Create data generators
+train_datagen = ImageDataGenerator(rescale=1./255)
+valid_datagen = ImageDataGenerator(rescale=1./255)
+
+train_data = train_datagen.flow(train_images, train_labels, batch_size=20)
+valid_data = valid_datagen.flow(valid_images, valid_labels, batch_size=20)
+
+# Model architecture
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(filters=10, kernel_size=3, activation="relu", input_shape=(28, 28, 1)),
+    tf.keras.layers.Conv2D(10, 3, activation="relu"),
+    tf.keras.layers.MaxPool2D(pool_size=2, padding="valid"),
+    tf.keras.layers.Conv2D(10, 3, activation="relu"),
+    tf.keras.layers.Conv2D(10, 3, activation="relu"),
+    tf.keras.layers.MaxPool2D(2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(10, activation="softmax")
+])
+
+model.compile(
+    loss="sparse_categorical_crossentropy",
+    optimizer=tf.keras.optimizers.Adam(),
+    metrics=["accuracy"]
+)
+
+# Train the model
+history = model.fit(
+    train_data,
+    epochs=5,
+    steps_per_epoch=len(train_data),
+    validation_data=valid_data,
+    validation_steps=len(valid_data)
+)
+
+
+
+
